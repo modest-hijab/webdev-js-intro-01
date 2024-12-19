7c230ab6dc24c58b5f2dc9d8e2304934d4a7bb33
modest-hijab <ayah1328@aol.com>
2024-12-18 21:26:28 -0500

    completed webdev js into o1 assignment

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+const red = "red";
+let one = 1;
+const blue = "blue";
+let two = 2;
+const white = "white";
+let three = 3;
+
+
+
+one = 4
